d35fc0cdc479f48ad3a10289fb3c904e663e6896
laps78 <9254384@mail.ru>
2023-02-04 15:45:23 +0300

    commit todos for android devstation

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,18 +1,30 @@
 import ListingItem from "./ListingItem";
+import { PropTypes } from 'prop-types';
 
 function Listing(data) {
   return (
     <div className="item-list">
       {
         Array.from(data.items).map((item) => {
-          const validateTitle = (item) => {if( item.title === undefined) return 'untitled product'};
-          const validateMainImage = (item) => {if( item.MainImage.url_570xN === undefined) return '\"#\"'};
+          // validators
+          const validateTitle = (item) => { if (item.title === undefined) return 'untitled product' };
+          // validate image link
+          const validateMainImage = (item) => {
+            const validatedImageURL = null;
+            if (item.MainImage.url_570xN === undefined) {
+              validatedImageURL = 'https://www.freeiconspng.com/uploads/no-image-icon-6.png';
+            } else {
+              validatedImageURL = item.MainImage.url_570xN;
+            }
+            return validatedImageURL;
+          };
+
           return (
             <ListingItem
               key={item.listing_id}
               title={validateTitle}
               url={item.url}
-              imageURL={validateMainImage}
+              imageURL={validateMainImage(item)}
               currency_code={item.currency_code}
               price={item.price}
               quantity={item.quantity}
@@ -25,9 +37,5 @@ function Listing(data) {
     </div>
   );
 }
-
-Listing.defaultProps = {
-  items: [],
-};
 
 export default Listing;
